375c2a480e165ae6db03b0da83a68c9477785ab0
trevortaulien <51167085+trevortaulien@users.noreply.github.com>
2022-05-03 10:28:05 -0400

    Completed 04 projects

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,64 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@ONES
+M=!D
+
+@ZEROS
+
+@KBD
+D=A
+@SCREENEND
+M=D
+
+@POLL
+0;JMP
+
+(BLACKOUT)
+@SCREENEND
+D=M		//get ending address
+@SCREENPOINTER
+D=D-M		//compute difference between current and end address
+@POLL
+D;JEQ		//if no difference then end of screen reached and exit to poll
+@ONES
+D=M		//gather ones to write
+@SCREENPOINTER
+A=M		//get address to write
+M=D		//write ones to address
+@SCREENPOINTER
+M=M+1		//increment pointer
+@BLACKOUT	
+0;JMP
+
+(WHITEOUT)	//Same as blackout just writing zeros instead of ones
+@SCREENEND
+D=M
+@SCREENPOINTER
+D=D-M
+@POLL
+D;JEQ	
+@ZEROS
+D=M
+@SCREENPOINTER
+A=M
+M=D
+@SCREENPOINTER
+M=M+1
+@WHITEOUT
+0;JMP
+
+(POLL)
+@SCREEN
+D=A		//Getting base address of screen
+@SCREENPOINTER
+M=D		//Storing screen base address
+@KBD
+D=M		//Getting value of keyboard
+@BLACKOUT
+D;JNE		//Blackout if key pressed
+@WHITEOUT
+D;JEQ		//Whiteout if no key pressed
+@POLL
+0;JMP
