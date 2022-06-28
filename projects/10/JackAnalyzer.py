7e89fd4812bca9b5a2a6c5528f64b4ee0133be92
trevortaulien <51167085+trevortaulien@users.noreply.github.com>
2022-06-28 17:43:20 -0400

    modified formatting of sourcejack

--- projects/10/JackAnalyzer.py
+++ projects/10/JackAnalyzer.py
@@ -11,7 +11,18 @@ class Analyzer:
         self.getJack()
 
     def tokenize(self):
-        pass
+        
+        tokenSleeve = []
+
+        tokenizer = Tokenizer(self.sourceJack)
+
+        i = 0
+        while(len(tokenizer.tokenBag) != 0):
+            token = tokenizer.nextToken()
+            tokenSleeve.append(token)
+            i +=1
+            if(i > 100):
+                quit()
 
     def compile(self):
         pass
@@ -60,6 +71,9 @@ class Analyzer:
                 temp = self.sourceJack[:blockCommentStart] + self.sourceJack[blockCommentEnd + 2:]
                 self.sourceJack = temp
 
+        def removeLeadingAndTrailingSpaces():
+            self.sourceJack = self.sourceJack.strip()
+
         dirContents = os.listdir(sys.argv[1])
         
         with open(sys.argv[1] + "Main.jack", 'r') as m:
@@ -80,34 +94,85 @@ class Analyzer:
         removeEmptyLines()
         flattenToString()
         removeBlockComments()
+        removeLeadingAndTrailingSpaces()
 
 class Tokenizer(Analyzer):
 
-    def __init__(self):
+    currentToken = ''
+
+    def __init__(self, sourceJack):
+        self.tokenBag = sourceJack
+        
+
+    def nextToken(self):
+        token = ['Replace', 'Me'] # format of some token is [type, token]
+        possibleToken = ''
+        tokenStatus = '-1'
+        index = 0
+
+        while(tokenStatus == '-1'):
+            possibleToken = possibleToken + self.tokenBag[index]
+            tokenStatus = self.ValidToken(possibleToken)
+            index += 1
+            if(index > 100):
+                quit()
+
+        self.tokenBag[index:]
+        token[0] = tokenStatus
+        token[1] = possibleToken
+        token[1].strip()
+        print(token)
+
+        return token
+
+    def ValidToken(self, possibleToken):
+        valid = '-1'
+        valid = self.keyword(possibleToken)
+        valid = self.symbol(possibleToken)
+        valid = self.identifier(possibleToken)
+        valid = self.intVal(possibleToken)
+        valid = self.stringVal(possibleToken)
+
+        return valid
+
+    def keyword(self, possibleToken):
+        keywords = {'class'  : 'KEYWORD', 'constructor': 'KEYWORD', 'function': 'KEYWORD', 'method': 'KEYWORD', 'field'  : 'KEYWORD',
+                    'static' : 'KEYWORD', 'var'        : 'KEYWORD', 'int'     : 'KEYWORD', 'char'  : 'KEYWORD', 'boolean': 'KEYWORD',
+                    'void'   : 'KEYWORD', 'true'       : 'KEYWORD', 'false'   : 'KEYWORD', 'null'  : 'KEYWORD', 'this'   : 'KEYWORD',
+                    'let'    : 'KEYWORD', 'do'         : 'KEYWORD', 'if'      : 'KEYWORD', 'else'  : 'KEYWORD', 'while'  : 'KEYWORD',
+                    'return' : 'KEYWORD'}
+
+        possibleToken.strip()
+
+        if(possibleToken == ' '):
+            return '-1'
+        elif(keywords[possibleToken]):
+            return 'KEYWORD'
+        else:
+            return '-1'
+
+    def symbol(self, possibleToken):
+        symbols = {'{' : 'SYMBOL', '}' : 'SYMBOL', '(' : 'SYMBOL', ')' : 'SYMBOL', '[' : 'SYMBOL', 
+                    ']' : 'SYMBOL', '.' : 'SYMBOL', ',' : 'SYMBOL', ';' : 'SYMBOL', '+' : 'SYMBOL', 
+                    '-' : 'SYMBOL', '*' : 'SYMBOL', '/' : 'SYMBOL', '&' : 'SYMBOL', '|' : 'SYMBOL', 
+                    '<' : 'SYMBOL', '>' : 'SYMBOL', '=' : 'SYMBOL', '~' : 'SYMBOL'}
+
+        possibleToken.strip()
+
+        if(possibleToken == ' '):
+            return '-1'
+        elif(symbols[possibleToken]):
+            return 'SYMBOL'
+        else:
+            return '-1'
+
+    def identifier(self, possibleToken):
         pass
 
-    def hasMoreTokens(self):
+    def intVal(self, possibleToken):
         pass
 
-    def advance(self):
-        pass
-
-    def tokenType(self):
-        pass
-
-    def keyWord(self):
-        pass
-
-    def symbol(self):
-        pass
-
-    def identifier(self):
-        pass
-
-    def intVal(self):
-        pass
-
-    def stringVal(self):
+    def stringVal(self, possibleToken):
         pass
 
 class CompilationEngine(Analyzer):
@@ -155,9 +220,9 @@ class CompilationEngine(Analyzer):
         pass
 
 vmMaker = Analyzer()
-vmMaker.tokenize()
-vmMaker.compile()
-vmMaker.outputVM()
+#vmMaker.tokenize()
+#vmMaker.compile()
+#vmMaker.outputVM()
 
 print(vmMaker.sourceJack)
 
