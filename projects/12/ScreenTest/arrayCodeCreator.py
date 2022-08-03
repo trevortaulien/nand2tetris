13cfa9e82c3e704f45eca81823d51f95ecc07a6f
trevortaulien <51167085+trevortaulien@users.noreply.github.com>
2022-08-03 10:03:09 -0400

    cleanup

---
+++ projects/12/ScreenTest/arrayCodeCreator.py
@@ -0,0 +1,5 @@
+# for x in range(0,512):
+#    print('let x_array[' + str(x) + '] = ' + str(int(x/16)) + ';')
+
+for y in range(0,256):
+    print('let y_array[' + str(y) + '] = ' + str(int(y * 32)) + ';')
